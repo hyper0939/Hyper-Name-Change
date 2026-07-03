e184b2d9d2a5edd66cb7da78697f312bdb623ffb
Hyper <144830674+hyper0939@users.noreply.github.com>
2026-07-03 14:52:05 +0200

    Added Github check

--- hyper_name-change/Code/server-side.lua
+++ hyper_name-change/Code/server-side.lua
@@ -220,3 +220,52 @@ end)
 
 --#region
 -- // Admin Command \\ --
+
+--#endregion
+
+--#region
+-- // Github \\ --
+local ResourceName = GetCurrentResourceName()
+
+local GITHUB_USER = "hyper0939"
+local GITHUB_REPO = "Hyper-Name-Change"
+local GITHUB_BRANCH = "main"
+
+local function GetCurrentVersion()
+    local manifest = LoadResourceFile(ResourceName, "fxmanifest.lua")
+    if not manifest then return "unkown" end
+    return manifest:match("version%s*['\"]([%d%.]+)['\"]") or "unknown"
+end
+
+local function CheckForUpdate()
+    local currentVersion = GetCurrentVersion()
+
+    PerformHttpRequest(("https://raw.githubusercontent.com/%s/%s/%s/hyper_name-change/fxmanifest.lua"):format(GITHUB_USER, GITHUB_REPO, GITHUB_BRANCH),
+    function (status, body, headers)
+        if status ~= 200 or not body then
+            print(("^3[%s] ^7 Update-Check failed (HTTP %s)"):format(ResourceName, status))
+            return
+        end
+
+        local latestVersion = body:match("version%s*['\"]([%d%.]+)['\"]") or "unknown"
+        if not latestVersion then
+            print(("^3[%s] ^7Could not read the GitHub version."):format(ResourceName))
+            return
+        end
+
+        if currentVersion == latestVersion then
+            print(("^9[%s] ^7Currently up to date. (v%s)"):format(ResourceName, currentVersion))
+        else
+            print(("^3[%s] ^7Update available! Installed ^1v%s ^7=> New: ^2v%s"):format(ResourceName, currentVersion, latestVersion))
+            print(("^3[%s] ^7Download https://github.com/%s/%s"):format(ResourceName, GITHUB_USER, GITHUB_REPO))
+        end
+    end, "GET", "", { ["Content-Type"] = "application/json" }
+    )
+end
+
+AddEventHandler("onResourceStart", function(resource)
+    if resource ~= ResourceName then return end
+
+    Citizen.SetTimeout(3000, CheckForUpdate)
+end)
+--#endregion
